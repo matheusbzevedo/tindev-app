50ee28055af791688323061d595f114d1fbd270f
Matheus Azevedo <matheusbzevedo@gmail.com>
2020-03-24 17:05:35 -0300

    adicionando o frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Login from './pages/Login'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
